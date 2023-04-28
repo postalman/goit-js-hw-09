686543a98eb7352eaa787a474a6aed8255855dd7
ChillyRide <sebastianmars78@gmail.com>
2023-04-28 03:09:52 +0300

    upd

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+  }
 
+  const btnStart = document.querySelector('[data-start]');
+  const btnStop = document.querySelector('[data-stop]');
+
+  btnStart.addEventListener('click', onBtnStart);
+  btnStop.addEventListener('click', onBtnStop);
+
+  btnStop.setAttribute('disabled', 'disabled');
+
+  function randomBodyColor () {
+    document.body.style.background = getRandomHexColor();
+  };
+
+    let intervalId;
+
+  function onBtnStart () {
+    intervalId = setInterval(randomBodyColor, 1000);
+    btnStart.setAttribute('disabled', 'disabled');
+    btnStop.removeAttribute('disabled');
+  };
+
+  function onBtnStop () {
+    clearInterval(intervalId);
+    btnStop.setAttribute('disabled', 'disabled');
+    btnStart.removeAttribute('disabled');
+  };
